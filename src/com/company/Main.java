b37bfe6372dee0e825d095104eb4d4b4e260f689
Kacper Warda <kacper.warda@logintegra.com>
2022-05-07 16:26:37 +0200

    dziedziczenie

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,40 +1,72 @@
 package com.company;
 
+import com.company.creatures.Human;
+import com.company.creatures.Pet;
+import com.company.device.Car;
+import com.company.device.Device;
+import com.company.device.Phone;
+
 public class Main {
 
     public static void main(String[] args) {
-        Animal dog = new Animal("canis");
+        Pet dog = new Pet("canis");
         dog.isAlive = true;
         dog.name = "Szarik";
 
         System.out.println(dog.name);
         System.out.println("species: " + dog.species + " name: " + dog.name);
 
-        Animal cat = new Animal("felis");
+        Pet cat = new Pet("felis");
         cat.isAlive = true;
         cat.name = "Sierściuch";
 
-        Human kacper = new Human();
+        System.out.println("Pet.toString(): " + cat);
 
-        kacper.setCar(new Car("bravo", "fiat"));
+        Human kacper = new Human();
+        kacper.age = 99;
+
+        kacper.setSalary(1000.0);
+        System.out.println(kacper.getSalary());
+
+        Car fiat = new Car("bravo", "fiat", 2021);
+        fiat.value = 500.0;
+
+        kacper.setCar(fiat);
         kacper.isAlive = false;
 
         kacper.firstName = "Kacper";
         kacper.lastName = "Warda";
         kacper.pet = dog;
-        kacper.mobile = new Phone();
+        kacper.mobile = new Phone("6s", "apple", 2018);
 
-        kacper.mobile.model = "6s";
-        kacper.mobile.producer = "apple";
         kacper.pet.feed();
 
-        kacper.setCar(new Car("passat", "vw"));
-        System.out.println(kacper.getCar().millage);
+
+        System.out.println(kacper.getCar());
+        System.out.println(kacper);
 
 
-        kacper.setSalary(1000.0);
-        System.out.println(kacper.getSalary());
+        Car pasat1 = new Car("pasat", "vw", 2001);
+        pasat1.value = 1200.0;
+        Car pasat2 = new Car("pasat", "vw", 2001);
+        pasat2.value = 1300.0;
 
+        System.out.println(pasat1.hashCode());
+        System.out.println(pasat2.hashCode());
+
+        if (pasat1.equals(pasat2)) {
+            System.out.println("to samo auto");
+        } else {
+            System.out.println("inne auto");
+        }
+
+        kacper.feed();
+        System.out.println(kacper.species);
+
+        System.out.println(pasat1 instanceof Car);
+        System.out.println(pasat1 instanceof Device);
+
+        System.out.println(pasat1);
 
     }
 }
